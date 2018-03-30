4683e3f52b47f703d74e6304d2204204391aa8bd
lingzy <lingzhengying@outlook.com>
2018-03-30 09:04:53 +0800

    create pipelines

--- bilibili/pipelines.py
+++ bilibili/pipelines.py
@@ -5,13 +5,32 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
-from . import settings
+from scrapy.exceptions import DropItem
+import settings
 
 class BilibiliPipeline(object):
-    def __init__(self):
-        self.client = MongoClient(settings.MONGO_HOST,settings.MONGO_PORT)
-        self.db = self.client[settings.DB_NAME]
-        self.sheet = self.db[settings.SHEET]
+    collection_name = 'bili_user'
+    def __init__(self,mongo_host,mongo_port,mongo_db):
+        self.mongo_host = mongo_host
+        self.mongo_port = mongo_port
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls,crawler):
+        return cls(
+            mongo_host=crawler.settings.get('MONGO_HOST')
+            mongo_port=crawler.settings.get('MONGO_PORT')
+            mongo_db=crawler.settings.get('DB_NAME')
+        )
+
+    def open_spider(self,spider):
+        self.client = MongoClient(self.mongo_host,self.mongo_port)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self,spider):
+        self.client.close()
+        
 
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(dict(item))
         return item
